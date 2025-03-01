e2b1408f7cfeccb3f16a27962f290ab43c32a91b
jnakht <jisan.cse.cu@gmail.com>
2025-03-01 17:58:14 +0600

    open close icon added

--- src/Components/Link/Link.jsx
+++ src/Components/Link/Link.jsx
@@ -1,9 +1,12 @@
 
-
+import PropTypes from 'prop-types';
 const Link = ({route}) => {
     return (
         <li><a href={route.path}>{route.name}</a></li>
     );
 };
 
+Link.propTypes = {
+    route: PropTypes.object.isRequired,
+}
 export default Link;
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import Link from "../Link/Link";
-
+import { IoMdMenu } from "react-icons/io";
+import { IoClose } from "react-icons/io5";
 
 const NavBar = () => {
     const routes = [
@@ -9,8 +11,18 @@ const NavBar = () => {
         { id: 4, path: "/contact", name: "Contact" },
         { id: 5, path: "/dashboard", name: "Dashboard" }
       ];
+
+      const [openClose, setOpenClose] = useState(true);
     return (
         <nav>
+            <div className="text-4xl md:hidden">
+                {
+                    (openClose===true) ? 
+                    <IoMdMenu onClick={() => setOpenClose(!openClose)} /> :
+                    <IoClose onClick={() => setOpenClose(!openClose)} />
+                }
+                
+            </div>
            <ul className="md:flex gap-10">
            {
                 routes.map(route => <Link key={route.id} route={route}></Link>)
