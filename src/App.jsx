13ba4f11715384ead75a5e2802625b2f5d85fb2a
jnakht <jisan.cse.cu@gmail.com>
2025-03-01 17:12:22 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import DaisyNav from './Components/DaisyNav/DaisyNav'
+import NavBar from './Components/NavBar/NavBar'
+
+function App() {
+  
+  
+
+  return (
+    <>
+      {/* <DaisyNav></DaisyNav> */}
+      <NavBar></NavBar>
+    </>
+  )
+}
+
+export default App
