45b8d54c503ecd65fce255b0c300d997d02cc0eb
jnakht <jisan.cse.cu@gmail.com>
2025-03-02 14:24:26 +0600

    data fetched using axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
 import BarChart from './Components/BarChart/BarChart'
 import DaisyNav from './Components/DaisyNav/DaisyNav'
@@ -6,9 +7,21 @@ import LineChart from './Components/LineChart/LineChart'
 import MultiLineChart from './Components/MultiLineChart/MultiLineChart'
 import NavBar from './Components/NavBar/NavBar'
 import PieC from './Components/PieC/PieC'
+import axios from 'axios'
 
 function App() {
   
+  const [phones, setPhones] = useState([]);
+  useEffect(() => {
+    // fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+    // .then(res => res.json())
+    // .then(data => console.log(data.data))
+
+    // fetch data using axios
+    axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
+    .then(data => console.log(data.data.data))
+
+  } ,[])
   
 
   return (
@@ -18,7 +31,7 @@ function App() {
       <LineChart></LineChart>
       <MultiLineChart></MultiLineChart>
       <BarChart></BarChart>
-      <PieC></PieC>
+      <PieC></PieC> 
     </>
   )
 }
