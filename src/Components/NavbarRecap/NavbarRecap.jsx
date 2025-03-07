a7548874b8ff53f5adff2ea5469aaa807e685db9
jnakht <jisan.cse.cu@gmail.com>
2025-03-07 23:41:03 +0600

    navbar recap done

--- src/Components/NavbarRecap/NavbarRecap.jsx
+++ src/Components/NavbarRecap/NavbarRecap.jsx
@@ -13,20 +13,23 @@ const NavbarRecap = () => {
       ];
 
       const [openClose, setOpenClose] = useState(true);
+      const handleOpenClose = () => {
+        setOpenClose(!openClose);
+      }
     return (
         <div>
-            <div>
-                <button onClick={() => setOpenClose(!openClose)}>
+            <div className="md:hidden">
+                   <button onClick={handleOpenClose}>
                      {
                         openClose ? <IoMenu className="text-4xl" />
                         :
                         <RxCross1 className="text-4xl"/>
                      }
-                </button>
-                 
-                 
-            </div>
-            <ul className="md:flex bg-gray-600 w-[200px] md:w-full rounded-xl md:rounded-none p-4 md:p-0">
+                    </button>
+                </div>
+            
+            <ul className={`md:flex bg-gray-600 w-[200px] md:w-full rounded-xl md:rounded-none p-4 md:p-0 duration-1000 ${!openClose ? 'absolute left-7' : 'absolute -left-80'}`}>
+                
             {
                 routes.map(route => <LinkRecap route={route}></LinkRecap>)
             }
